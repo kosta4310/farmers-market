8c086ccb5df6e2253c7c8c7a81b7197a27dd23a8
Pavlov Kostyantyn <realanka77@gmail.com>
2023-08-10 19:02:45 +0300

    kos-dev: change config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  textColor: {
+    default: "#00A919",
+  },
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
