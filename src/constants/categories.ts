5a78886f7c60e851ecec57d6701e194221dcf898
ww_side <daniil.metelia@gmail.com>
2023-08-11 17:12:44 +0300

    dan-dev: added constants

---
+++ src/constants/categories.ts
@@ -0,0 +1,10 @@
+export const categories = [
+  { id: 1, title: `М'ясо` },
+  { id: 2, title: 'Молочна продукція' },
+  { id: 3, title: 'Сири' },
+  { id: 4, title: 'Фрукти та овочі' },
+  { id: 5, title: 'Яйця' },
+  { id: 6, title: 'Бакалія' },
+  { id: 7, title: 'Напої' },
+  { id: 8, title: 'Кравтофі продукти' },
+];
---
+++ src/constants/information.ts
@@ -0,0 +1,6 @@
+export const information = [
+  { id: 1, title: 'Про нас', route: '/about' },
+  { id: 2, title: 'Правила та умови', route: '/rules' },
+  { id: 3, title: 'Доставка', route: '/delivery' },
+  { id: 4, title: 'Оплата', route: '/payment' },
+];
