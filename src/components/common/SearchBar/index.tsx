e65e26509a52d47aae3edf51d2657956d4d86128
ww_side <daniil.metelia@gmail.com>
2023-08-11 17:19:28 +0300

    dan-dev: improved SearchBar

--- src/components/common/SearchBar/index.tsx
+++ src/components/common/SearchBar/index.tsx
@@ -1,12 +1,27 @@
-import { FC } from "react";
+import React, { FC, useState } from 'react';
+import search from '../../../assets/icons/user-bar/search.svg';
 
 const SearchBar: FC = () => {
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value);
+  };
+
   return (
-    <input
-      className="rounded-md focus:outline-none
-      focus:shadow-outline"
-      type="text"
-    />
+    <label
+      htmlFor="search"
+      className="flex gap-2 bg-white rounded-md shadow-md w-1/3 px-3 py-1"
+    >
+      <img src={search} alt="search" />
+      <input
+        id="search"
+        className="rounded-md w-1/3 focus:outline-none focus:shadow-outline text-opacity-50 border-opacity-50"
+        type="text"
+        value={searchText}
+        onChange={handleSearchChange}
+      />
+    </label>
   );
 };
 
