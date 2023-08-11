b0ec277635cd7911e35066e16e8b5a2bf0203ffc
ww_side <daniil.metelia@gmail.com>
2023-08-11 17:18:59 +0300

    dan-dev: refactoring Header

--- src/components/shared/Header/NavContacts/index.tsx
+++ src/components/shared/Header/NavContacts/index.tsx
@@ -1,12 +1,18 @@
-import { FC } from "react";
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
+import { information } from '../../../../constants/information.ts';
 
 const NavContacts: FC = () => {
   return (
     <ul className="flex items-center justify-center gap-8 mt-3 mb-2">
-      <li>Про нас</li>
-      <li>Правила та умови</li>
-      <li>Доставка</li>
-      <li>Оплата</li>
+      {information.map((item) => (
+        <li
+          key={item.id}
+          className="hover:text-default cursor-pointer transition duration-300 ease-in-out"
+        >
+          <Link to={item.route}>{item.title}</Link>
+        </li>
+      ))}
     </ul>
   );
 };
--- src/components/shared/Header/UserBar/index.tsx
+++ src/components/shared/Header/UserBar/index.tsx
@@ -1,19 +1,15 @@
-import { FC } from "react";
-import logo from "../../../../assets/img/user-panel-logo.svg";
-import user from "../../../../assets/icons/user.svg";
-import favorites from "../../../../assets/icons/favorites.svg";
-import basket from "../../../../assets/icons/basket.svg";
-import SearchBar from "../../../common/SearchBar";
+import { FC } from 'react';
+import SearchBar from '../../../common/SearchBar';
+import logo from '../../../../assets/img/user-panel-logo.svg';
+import user from '../../../../assets/icons/user-bar/user.svg';
+import favorites from '../../../../assets/icons/user-bar/favorites.svg';
+import basket from '../../../../assets/icons/user-bar/basket.svg';
 
 const UserPanel: FC = () => {
   return (
     <div className="flex justify-between items-center px-10 py-1.5 bg-default shadow-md">
-      <span>
-        <img src={logo} alt="logo" />
-      </span>
-      <span>
-        <SearchBar />
-      </span>
+      <img src={logo} alt="logo" />
+      <SearchBar />
       <span className="flex gap-6 items-center cursor-pointer">
         <img src={user} alt="user" />
         <img src={favorites} alt="favorites" />
