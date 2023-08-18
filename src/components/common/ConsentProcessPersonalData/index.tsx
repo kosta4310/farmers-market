702407714f4c1aca053e6b955de27317a8ea24e9
ww_side <daniil.metelia@gmail.com>
2023-08-18 14:10:56 +0300

    dan-dev: added 'consent to process personal data'

---
+++ src/components/common/ConsentProcessPersonalData/index.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+const ConsentProcessPersonalData: FC = () => {
+  return (
+    <span className="text-xs text-gray-500">
+      Реєструючись, ви погоджуєтеся на зберігання і використання наданих вами
+      особистих даних відповідно до чинного законодавства України про
+      недоторканність особистої інформації.
+    </span>
+  );
+};
+
+export default ConsentProcessPersonalData;
