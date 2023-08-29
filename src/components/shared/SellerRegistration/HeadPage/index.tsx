99bbd5ec0d3acb4d63affba660c979b3e9c56c60
Pavlov Kostyantyn <realanka77@gmail.com>
2023-08-29 22:49:53 +0300

    kos-dev: add pageNavigation

--- src/components/shared/SellerRegistration/HeadPage/index.tsx
+++ src/components/shared/SellerRegistration/HeadPage/index.tsx
@@ -3,6 +3,7 @@ import HeadPageContent from '../HeadPageContent';
 import ButtonArrow from '../../../common/ButtonArrow';
 import { useAppDispatch, useAppSelector } from '../../../../hooks/redux';
 import { registrationSlice } from '../../../../store/reducers/registrationSlice';
+import PageNavigation from '../PageNavigation';
 
 const HeadPage: FC = () => {
   const dispatch = useAppDispatch();
@@ -19,6 +20,7 @@ const HeadPage: FC = () => {
 
   return (
     <div className="flex flex-col gap-6 mb-7">
+      <PageNavigation page={registrationPage} />
       <HeadPageContent />
       <div className="flex items-center justify-between">
         <ButtonArrow
