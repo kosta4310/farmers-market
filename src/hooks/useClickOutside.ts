156113b58230758c8ba5d7a4cdfd5695434f140d
Pavlov Kostyantyn <realanka77@gmail.com>
2023-09-22 21:06:16 +0300

    kos-dev: add useClickOutside hook

---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,19 @@
+import { RefObject, useEffect } from 'react';
+
+const useClickOutside = (ref: RefObject<HTMLElement>, callback: () => void) => {
+  const handleClick = (event: MouseEvent) => {
+    if (ref.current && !ref.current.contains(event.target as Node)) {
+      callback();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClick);
+    return () => {
+      document.removeEventListener('mousedown', handleClick);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+};
+
+export default useClickOutside;
