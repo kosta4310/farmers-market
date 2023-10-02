f1a9cc12e1fa4a5e91fcf935c6ed8fad96396584
Pavlov Kostyantyn <realanka77@gmail.com>
2023-10-02 10:28:42 +0300

    kos-dev: add color theme

--- src/api/config/index.ts
+++ src/api/config/index.ts
@@ -1,7 +1,7 @@
 const BASE = 'https://test-nest-deploy-2zrrw65jva-lz.a.run.app';
 // const BASE = 'http://127.0.0.1:4000';
 
-const BUYERS_SIGNUP = `${BASE}/auth/buyers/register`;
+const BUYERS_SIGNUP = `${BASE}/auth/register/buyer`;
 // const BUYERS_SIGNIN = `${BASE}/buyers/login`;
 const CONFIRM_EMAIL = `${BASE}/auth/verify-email`;
 const AUTH_SIGNIN = `${BASE}/auth/login`;
