5d47f77251cf6b62794787252317937e850fc809
oom22 <oo@exdst.com>
2023-11-20 13:55:39 +0200

    changed Base Path

--- src/api/config/index.ts
+++ src/api/config/index.ts
@@ -1,5 +1,10 @@
-const BASE = 'https://test-nest-deploy-2zrrw65jva-lz.a.run.app';
+// const BASE = 'https://test-nest-deploy-2zrrw65jva-lz.a.run.app';
 // const BASE = 'http://127.0.0.1:4000';
+
+
+const BASE = 'https://marketplace-back-endat.onrender.com';
+
+
 
 const BUYERS_SIGNUP = `${BASE}/auth/register/buyer`;
 const SELLER_SIGNUP = `${BASE}/auth/register/seller`;
