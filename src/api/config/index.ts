29c7f8886863911b85416da380e6360671dbf168
oom22 <oo@exdst.com>
2023-11-20 11:42:33 +0200

    changed Base Path

--- src/api/config/index.ts
+++ src/api/config/index.ts
@@ -1,5 +1,5 @@
-// const BASE = 'https://test-nest-deploy-2zrrw65jva-lz.a.run.app';
-const BASE = 'http://127.0.0.1:4000';
+const BASE = 'https://test-nest-deploy-2zrrw65jva-lz.a.run.app';
+// const BASE = 'http://127.0.0.1:4000';
 
 const BUYERS_SIGNUP = `${BASE}/auth/register/buyer`;
 const SELLER_SIGNUP = `${BASE}/auth/register/seller`;
