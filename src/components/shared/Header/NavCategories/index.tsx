e06226b1b8fe3f4ba6638c37a20ea33e7967ef48
SerheyPopov <serheypopov1994@gmail.com>
2023-12-10 18:52:47 +0200

    add sub categories

--- src/components/shared/Header/NavCategories/index.tsx
+++ src/components/shared/Header/NavCategories/index.tsx
@@ -1,18 +1,106 @@
-import { FC } from 'react';
-import { categories } from '../../../../constants/categories.ts';
+import { FC, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import { subCategory } from '../../../../constants/categories.ts';
+import { mainCategories } from '../../../../constants/categories.ts';
+import Close from '../../../../assets/icons/subProd/close.svg';
+
+type SubCat = {
+  categoryId: number;
+  value: string;
+  label: string;
+  image: string;
+}[];
 
 const NavCategories: FC = () => {
+  const [subContainer, setSubConteiner] = useState<boolean>(false);
+  const [subList, setSubList] = useState<SubCat>([]);
+  const [activeId, setActiveId] = useState<number>(0);
+  const [subName, setSubName] = useState<string>('');
+
+  const openSubCategories = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const target = e.target as HTMLButtonElement;
+    setSubConteiner(true);
+    setSubName(target.innerText);
+    setSubList(subCategory.filter(el => el.categoryId === Number(target.id)));
+
+    window.onscroll = function () {
+      window.scrollTo(0, 0);
+    };
+  };
+
+  function closeSubCategories() {
+    setSubConteiner(false);
+    setActiveId(0);
+    window.onscroll = window.onload;
+  }
+
   return (
-    <ul className="flex items-center justify-center gap-20 py-3 font-semibold shadow-md">
-      {categories.map((item) => (
-        <li
-          key={item.id}
-          className="hover:text-default cursor-pointer transition duration-300 ease-in-out"
-        >
-          {item.title}
-        </li>
-      ))}
-    </ul>
+    <div className="">
+      <div className="relative z-20 bg-white">
+        <ul className="flex items-center justify-center gap-20 py-3 font-semibold shadow-md ">
+          {mainCategories.map(({ id, label }) => (
+            <li
+              onClick={() => setActiveId(id)}
+              key={id}
+              className={
+                activeId === id
+                  ? 'text-default cursor-pointer transition duration-300 ease-in-out'
+                  : 'hover:text-default cursor-pointer transition duration-300 ease-in-out'
+              }
+            >
+              <button
+                type="button"
+                onClick={openSubCategories}
+                id={id.toString()}
+              >
+                {label}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {subContainer && (
+        <div className=" absolute top-0 z-10 h-full overflow-hidden w-full bg-black bg-opacity-50 ">
+          <div className="h-[289px] bg-white mt-[152px] pt-6 px-10">
+            <div className="flex mb-6 justify-between">
+              <h1 className="text-3xl leading-6 font-semibold text-secondary">
+                {subName}
+              </h1>
+              <button type="button" onClick={closeSubCategories}>
+                <svg className="w-6 h-6 stroke-disabled hover:stroke-text_com">
+                  <use href={Close + '#close'} />
+                </svg>
+              </button>
+            </div>
+
+            <ul className=" flex items-center  gap-16">
+              {subList?.map(item => (
+                <li key={item.value}>
+                  <div className="bg-card_background  h-40 rounded-md border-2 border-card_background hover:border-secondary">
+                    <NavLink
+                      to={'/product'}
+                      onClick={closeSubCategories}
+                      state={item}
+                    >
+                      <img
+                        src={item.image}
+                        alt="product"
+                        className="object-cover h-[100px] w-[100px] mb-4 rounded-t-md"
+                      />
+                      <p className="w-[100px] text-center text-sm text-text_com">
+                        {item.label}
+                      </p>
+                    </NavLink>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
--- src/constants/categories.ts
+++ src/constants/categories.ts
@@ -8,3 +8,254 @@ export const categories = [
   { id: 7, title: 'Напої' },
   { id: 8, title: 'Кравтофі продукти' },
 ];
+
+export const mainCategories = [
+  { id: 1, value: '1', label: `М'ясо` },
+  { id: 2, value: '2', label: 'Молочна продукція' },
+  { id: 3, value: '3', label: 'Сири' },
+  { id: 4, value: '4', label: 'Фрукти та овочі' },
+  { id: 5, value: '5', label: 'Яйця' },
+  { id: 6, value: '6', label: 'Бакалія' },
+  { id: 7, value: '7', label: 'Напої' },
+  { id: 8, value: '8', label: 'Кравтофі продукти' },
+];
+
+export const subCategory = [
+  {
+    categoryId: 1,
+    value: '1',
+    label: 'Яловичина',
+    image:
+      'https://klopotenko.com/wp-content/uploads/2023/09/depositphotos_298340776_s-img.jpg',
+  },
+  {
+    categoryId: 1,
+    value: '2',
+    label: 'Свинина',
+    image:
+      'https://infokava.com/uploads/posts/2015-11/1447849326_26692_1392786861_1.jpg',
+  },
+  {
+    categoryId: 1,
+    value: '3',
+    label: 'Курка',
+    image:
+      'https://i.obozrevatel.com/food/recipemain/2019/8/14/meatproductsroast483124.jpg?size=636x424',
+  },
+  {
+    categoryId: 1,
+    value: '4',
+    label: 'Індичка',
+    image:
+      'https://myasnuyray.com.ua/wp-content/uploads/2019/12/24-12-19-5-2-666x445.jpg',
+  },
+  {
+    categoryId: 1,
+    value: '5',
+    label: 'Баранина',
+    image:
+      'https://lambico.com.ua/files/originals/lopatka-beskostnaya-vysshij-sort-baranina.jpg.pagespeed.ce.Xoz-1QsKa3.jpg',
+  },
+  {
+    categoryId: 1,
+    value: '6',
+    label: 'Стейки',
+    image:
+      'https://myastoriya.com.ua/upload/medialibrary/bc4/bc4f0a42a02694517212bcca653f0ac6.jpg',
+  },
+  {
+    categoryId: 2,
+    value: '1',
+    label: 'Коров’яче молоко',
+    image:
+      'https://img02.flagma.ua/photo/prodam-korove-moloko-kievskaya-obl-15089934_big.jpg',
+  },
+  {
+    categoryId: 2,
+    value: '2',
+    label: 'Козяче молоко',
+    image:
+      'https://burshtyn-rada.if.gov.ua/wp-content/uploads/2023/02/397008_1.jpg',
+  },
+  {
+    categoryId: 2,
+    value: '3',
+    label: 'Сир',
+    image: 'https://i.obozrevatel.com/news/2023/7/20/sir.jpg?size=1944x924',
+  },
+  {
+    categoryId: 2,
+    value: '4',
+    label: 'Вершки',
+    image:
+      'https://klopotenko.com/wp-content/uploads/2020/05/Yak-zbyty-vershky_siteWebUkr.jpg?v=1589367362',
+  },
+  {
+    categoryId: 2,
+    value: '5',
+    label: 'Вершкове масло',
+    image: 'https://rud.ua/uploads/food/content/5bd6f35fd1c96.jpg',
+  },
+  {
+    categoryId: 3,
+    value: '1',
+    label: 'Свіжі сири',
+    image:
+      'https://life.pravda.com.ua/images/doc/3/b/3bcdb33-svariophoto-depositphotos.jpg',
+  },
+  {
+    categoryId: 3,
+    value: '2',
+    label: 'М’які сири',
+    image:
+      'https://storinka.com.ua/storage/source/13/YogcOEVR2e02AWyNR9mEXoFphNOtBfUB.png',
+  },
+  {
+    categoryId: 3,
+    value: '3',
+    label: 'Напівтверді сири',
+    image:
+      'https://agropererobka.com.ua/media/images/syry_polutverdye/Kupit-polutverdyy-syr-TM-Shchedrik.jpg.pagespeed.ce.OpH8Sx0fN_.jpg',
+  },
+  {
+    categoryId: 3,
+    value: '4',
+    label: 'Тверді сири',
+    image:
+      'https://milkvisit.com/wp-content/uploads/2020/09/close-up-tasty-swiss-cheese-on-the-table-with-honey_23-2148430147-e1599662537377.jpg',
+  },
+  {
+    categoryId: 3,
+    value: '5',
+    label: 'Голубі сири',
+    image:
+      'https://health.24tv.ua/resources/photos/news/201707/846843.jpg?v=1658993720000',
+  },
+  {
+    categoryId: 4,
+    value: '1',
+    label: 'Фрукти',
+    image:
+      'https://www.rbc.ua/static/img/1/6/167a908b_97c2_4f0b_98de_325b6ea01545_1200x675.jpg',
+  },
+  {
+    categoryId: 4,
+    value: '2',
+    label: 'Овочі',
+    image:
+      'https://agroportal.ua/storage/media/uploads/articles/365102d-food-1209503-960-720.jpg',
+  },
+  {
+    categoryId: 4,
+    value: '3',
+    label: 'Зелень',
+    image:
+      'https://ukrainianwall.com/crops/25dfe7/620x0/1/0/2023/11/13/ZC9SaM6uhQbuGbOr63xVgadArjlgUOjoPejjNUh1.png',
+  },
+  {
+    categoryId: 5,
+    value: '1',
+    label: 'Курячі яйця',
+    image:
+      'https://news24.in.ua/wp-content/uploads/shutterstock_404830774-750x400-1.jpg',
+  },
+  {
+    categoryId: 5,
+    value: '2',
+    label: 'Перепелині яйця',
+    image:
+      'https://images.unian.net/photos/2022_11/thumb_files/1200_0_1667904400-2882.jpg',
+  },
+  {
+    categoryId: 6,
+    value: '1',
+    label: 'Крупи',
+    image:
+      'https://propozitsiya.com/sites/default/files/field/image/901761.jpeg',
+  },
+  {
+    categoryId: 6,
+    value: '2',
+    label: 'Паста',
+    image:
+      'https://vokrugsveta.ua/wp-content/uploads/2018/05/shutterstock_112494824.jpg',
+  },
+  {
+    categoryId: 6,
+    value: '3',
+    label: 'Спеції',
+    image: 'https://etnoxata.com.ua/image/catalog/blog/002020/001/036.jpeg',
+  },
+  {
+    categoryId: 6,
+    value: '4',
+    label: 'Борошно',
+    image:
+      'https://storinka.com.ua/storage/source/12/bwtPqQmPsAU9vBzZd5bYrP7ZGudjiFC2.jpg',
+  },
+  {
+    categoryId: 6,
+    value: '5',
+    label: 'Масло',
+    image: 'https://content2.rozetka.com.ua/goods/images/big/332000153.jpg',
+  },
+  {
+    categoryId: 7,
+    value: '1',
+    label: 'Чай',
+    image:
+      'https://www.rbc.ua/static/img/g/l/glass_teapot_with_tea_wooden_board_1200x675.jpg',
+  },
+  {
+    categoryId: 7,
+    value: '2',
+    label: 'Кава',
+    image: 'https://brayval-coffee.com.ua/content/uploads/images/5_main.jpg',
+  },
+  {
+    categoryId: 7,
+    value: '3',
+    label: 'Вода',
+    image: 'https://www.rbc.ua/static/img/p/i/pixabay_voda_1_1200x675.jpg',
+  },
+  {
+    categoryId: 7,
+    value: '4',
+    label: 'Сік ',
+    image: 'https://www.rbc.ua/static/img/p/i/pixabay_com__2__114_1200x675.jpg',
+  },
+  {
+    categoryId: 7,
+    value: '5',
+    label: 'Безалкогольні напої ',
+    image: 'https://cdn.abo.media/upload/article/sgaak2bbov5pcwuah6ug.jpg',
+  },
+  {
+    categoryId: 8,
+    value: '1',
+    label: 'Солоне',
+    image: 'https://img02.flagma.ua/photo/solonina-5058452_big.jpg',
+  },
+  {
+    categoryId: 8,
+    value: '2',
+    label: 'Солодке',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsJdcLaxRQqD7lKhXQG6uwFCOwdhbUZ00_xw&usqp=CAU',
+  },
+];
+
+export const unit = [
+  {
+    value: 'kg',
+    label: 'Кілограм',
+  },
+  {
+    value: 'litre',
+    label: 'Літр',
+  },
+  {
+    value: 'pcs',
+    label: 'Штука',
+  },
+];
